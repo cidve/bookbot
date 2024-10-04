af38b2771d49579fe97fa4fe5ef20c1e4f79b352
cidve <cidveragabriel@gmail.com>
2024-10-04 20:22:08 -0300

    add count_letters and order_dict

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ def main():
     # book_path = test_path
     text = get_text(book_path)
     number_of_words = count_words(text)
-    print(number_of_words)
-    return (number_of_words)
+    letters_count = count_letters(text)
+    letters_ordered = order_dict(letters_count)
+    print(letters_ordered)
+    # return (number_of_words)
 
 def get_text(path):
     with open(path) as f:
@@ -15,6 +17,22 @@ def get_text(path):
 def count_words(text):
     text_split = text.split()
     return len(text_split)
+
+def count_letters(text):
+    text = text.lower()
+    text_split = text.split()
+    letter_count = {}
+    for word in text_split:
+        for i in word:
+            if i not in letter_count:
+                letter_count[i] = 1
+            else:
+                letter_count[i] += 1
+    return letter_count
+
+def order_dict(dictionary):
+    return dict(sorted(dictionary.items(), key=lambda item: item[1], reverse=True))
+
 
 # tests
 if __name__ == '__main__':
